330cba196413ed59940fcfb60a9521403de97098
Edgar Velazquez <edantonio505@gmail.com>
2017-02-20 20:28:27 -0500

    first commit

---
+++ components/skills.js
@@ -0,0 +1,87 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} from 'react-native';
+
+var base = 'http://dev.talentlodge.com/api/';
+
+export default class Skills extends Component 
+{
+  constructor(props) {
+     super(props);
+     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+     this.state = {
+       dataSource: ds.cloneWithRows([]),
+       loaded: false
+     };
+     getTracks(this);
+   }
+
+
+   renderLoadingView() {
+     return (
+       <View>
+         <Text>
+           loading tracks
+         </Text>
+       </View>
+     );
+   }
+
+
+   renderView(){
+     return (
+       <View>
+         <Text>All Tanyas carita soundcloud tracks</Text>
+         <ListView dataSource={this.state.dataSource} renderRow={(rowData) => <Text>{rowData.title}</Text>}/>
+       </View>
+     );
+   }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    } else {
+      return this.renderView(this);
+    }   
+  }
+}
+
+
+
+
+
+// Ajax to get all the tracks
+function getTracks(element){
+  fetch(base+'tracks/1',{
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
+  })
+   .then((response) => response.json())
+   .then((tracks) => {
+    console.log(tracks);
+
+    element.setState({
+      dataSource: element.state.dataSource.cloneWithRows(tracks),
+      loaded: true
+    });
+   })
+   .catch((error) => {
+     console.error(error);
+  })
+  .done();
+}
+
---
+++ index.android.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Landing from './components/landing';
+import Skills from './components/skills';
+
+
+export default class talentlodge extends Component 
+{
+  render() {
+    return (
+      <View>
+        <Landing />
+        <Skills/>
+      </View>
+    );
+
+  }
+}
+
+AppRegistry.registerComponent('talentlodge', () => talentlodge);

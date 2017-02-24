976b9cd724743e16712d8492abdf35764c0add80
Edgar Velazquez <edantonio505@gmail.com>
2017-02-24 00:58:19 -0500

    filtering through skills

--- index.android.js
+++ index.android.js
@@ -11,14 +11,102 @@ import {
   Text,
   View,
   Image,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  TextInput,
+  ListView
 } from 'react-native';
-import Landing from './components/landing';
-import Skills from './components/skills';
-import Searchbox from './components/searchbox';
-  
+
+let border_radius =  8;
+
+
+/*============================================= 
+          skill api base config
+===============================================*/
+  var base = 'http://dev.talentlodge.com/api/skills';
+  // var base = 'http://192.168.0.13/api/skills';
+// -----------------------------------------------
+
+ 
 export default class talentlodge extends Component 
-{
+{ 
+  constructor() {
+    super(); 
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      searchText: '',
+      tracks: {},
+      dataSource: ds.cloneWithRows([])
+    };
+    this.getTracks()
+  }
+
+
+
+  updateSearch(event)
+  {   
+      this.setState({
+          searchText: event.nativeEvent.text
+      })
+
+      this.getTracks();
+  }
+
+
+
+
+
+  filterTracks(searchText, tracks) {
+    let text = searchText.toLowerCase();
+    return tracks.filter(
+      (track) => {
+        return track.name.toLowerCase().indexOf(text) !== -1;
+      }
+    );
+    
+  }
+
+
+
+
+
+
+  
+  getTracks(){
+    fetch(base,{
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+     .then((response) => response.json())
+     .then((tracks) => {
+        
+          let filtered = this.filterTracks(this.state.searchText, tracks)
+        // 
+          this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(filtered)
+          });
+     })
+     .catch((error) => {
+       console.error(error);
+    })
+    .done();
+  }
+
+
+  renderList(rowData) {
+    return (
+      <Text style={styles.listItems}>
+        {rowData.name}
+      </Text>
+
+    );
+   }
+
+
+
+
   render() {
     return (
 
@@ -30,10 +118,23 @@ export default class talentlodge extends Component
           </View>
 
         <KeyboardAvoidingView behavior="padding">
-          <Searchbox />
+          <View  style={styles.inputContainer}>
+              <TextInput 
+                  underlineColorAndroid="rgba(0, 0, 0, 0)" 
+                  style={styles.searchBox} 
+                  placeholder="Search for a skill" 
+                  onChange={this.updateSearch.bind(this)}
+              />
+          </View>
         </KeyboardAvoidingView>
+
+
         <View style={styles.listContainer} >
-          <Skills />
+          <View  style={styles.listContainer}>
+             <ListView 
+              enableEmptySections={true}
+              dataSource={this.state.dataSource} renderRow={this.renderList}/>
+           </View>
         </View>
       </View>
     );
@@ -69,7 +170,37 @@ var styles = StyleSheet.create({
     width: null,
     height: null,
     resizeMode: 'contain'
-  }
+  },
+    inputContainer: {
+        paddingBottom:10,
+        paddingLeft: 20,
+        paddingRight: 20
+    },
+
+  searchBox:{
+        backgroundColor:'rgb(255,255,255)',
+        borderTopRightRadius: border_radius,
+        borderTopLeftRadius: border_radius,
+        borderBottomLeftRadius: border_radius,
+        borderBottomRightRadius: border_radius,
+        textAlign:'center'
+    },
+    listContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems:'center',
+    },
+    text: {
+      color: 'white',
+      fontSize: 20
+    },
+    listItems: {
+      color: 'white',
+      textAlign:'center',
+      fontSize: 20,
+      margin:10,
+    }
 });
 
 

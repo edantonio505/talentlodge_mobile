0ef8e992227e01dd4f9dc3a21d40c2c9bd4da22c
Edgar Velazquez <edgarvelazquez@Edgars-MacBook-Air.local>
2017-03-09 00:49:22 -0500

    opening new scene and styling

--- components/results.js
+++ components/results.js
@@ -3,7 +3,9 @@ import {
   StyleSheet,
   Text,
   View,
-  ListView
+  ListView,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
 
@@ -22,40 +24,70 @@ export default class Results extends Component
 
 
   renderUsersRow(rowData){
-    console.log(rowData);
+    console.log(rowData._source.avatar);
+    let imageURI = 'https://i.vimeocdn.com/portrait/58832_300x300.jpg';
     return (
-      <View>
-        <Text style={styles.words}>{rowData._source.name} {rowData._source.skills.toString()}</Text>
-      </View>
-
+        <TouchableOpacity style={styles.listButtons} >
+          <View style={{flex:1, flexDirection: 'row'}}>
+            <View style={{ width: 60, height: 60, flex:1, padding:10, flexDirection: 'row'}}>
+              <Image style={{width: 60, height: 60, borderRadius: 30}} resizeMode={'cover'} source={{uri: String(rowData._source.avatar) }} />
+              <Text style={styles.text}>{rowData._source.name}</Text>
+              <View>
+                <Text style={styles.second_text}>{String(rowData._source.skills.toString()).replace(/,/g , " ")}</Text>
+              </View>
+            </View>
+          </View>
+        </TouchableOpacity>
     );
   }
 
   render (){  
     return (
-      <View style={styles.viewContainer}>
-        <ListView
-          enableEmptySections={true}
-          dataSource={this.state.dataSource}
-          renderRow={this.renderUsersRow.bind(this)}
-        />
-      </View>
+        <View style={styles.container}>
+          <View>
+            <ListView
+              enableEmptySections={true}
+              dataSource={this.state.dataSource}
+              renderRow={this.renderUsersRow.bind(this)}
+            />
+          </View>
+        </View>
     );
   }
 }
 
 
-
 var styles = StyleSheet.create({
-  viewContainer : {
+  container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    backgroundColor: '#00B2EE',
-    alignItems: 'center'
+    flexDirection: 'column', 
+    justifyContent: 'center', 
+    backgroundColor: 'black'
   },
-  words: {
-    paddingTop: 20,
-    color: 'white'
+
+  text:{
+    color:'white',
+    fontSize: 20,
+    paddingLeft: 10
+  },
+  second_text: {
+    color:'white',
+    position:'relative',
+    fontSize: 10,
+    top:40,
+    left: -55
+  },
+  avatars:{
+    width:70
+  },
+  listButtons:{
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    height:80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomColor: 'white',
+    borderBottomWidth: 0.5
   }
 });
+
+
